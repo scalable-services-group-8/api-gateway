b9448503a703c9e667407851fd3b41da2d2d3255
Nitesh Morajkar <nitesh@morajkar.com>
2023-10-30 00:15:53 +0530

    Commit

--- api_gateway_service/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    port: 3000,
-	endpoints: {
-		user_microservice: "http://user-microservice:3001",
-        booking_microservice: "http://booking-microservice:3002",
-        transaction_microservice: "http://transaction-microservice:3003"
-	}
-};
